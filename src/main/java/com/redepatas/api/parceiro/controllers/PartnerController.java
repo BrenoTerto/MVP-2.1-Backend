39bc9f128b37b84107c707f821a0741d132ce07e
Ian Diniz de Oliveira <iandiniz653@gmail.com>
2025-08-02 19:12:54 -0300

    DTO partner commit

--- src/main/java/com/redepatas/api/parceiro/controllers/PartnerController.java
+++ src/main/java/com/redepatas/api/parceiro/controllers/PartnerController.java
@@ -1,6 +1,7 @@
 package com.redepatas.api.parceiro.controllers;
 
 import com.redepatas.api.cliente.dtos.ReponseLoginDto;
+import com.redepatas.api.cliente.dtos.petDtos.NewPetDto;
 import com.redepatas.api.cliente.models.AuthenticationDTO;
 import com.redepatas.api.infra.security.TokenService;
 import com.redepatas.api.parceiro.dtos.PartnerDtos.PartnerDto;
@@ -18,6 +19,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
@@ -35,31 +37,19 @@ public class PartnerController {
     @Autowired
     private TokenService tokenService;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @PostMapping("/create")
     public ResponseEntity<String> createPartner(
-            @RequestPart(value = "partnerData", required = false) String partnerDataJson,
+            @RequestPart(value = "partnerData", required = false) @Validated PartnerRecordDto partnerDataJson,
             @RequestPart(value = "image", required = false) MultipartFile image)
             throws com.fasterxml.jackson.core.JsonProcessingException {
-
-        // Validar se o partnerData foi fornecido
-        if (partnerDataJson == null || partnerDataJson.trim().isEmpty()) {
-            throw new IllegalArgumentException("Os dados do parceiro são obrigatórios.");
-        }
 
         // Validar se a imagem foi fornecida
         if (image == null || image.isEmpty()) {
             throw new IllegalArgumentException("A imagem é obrigatória para o cadastro do parceiro.");
         }
 
-        // Converter JSON string para DTO - JsonProcessingException será tratada pelo
-        // ExceptionHandler
-        PartnerRecordDto partnerDto = objectMapper.readValue(partnerDataJson, PartnerRecordDto.class);
-
         // Chamar o service - exceções são tratadas pelo PartnerExceptionHandler
-        String savedPartner = partnerService.createPartner(partnerDto, image);
+        String savedPartner = partnerService.createPartner(partnerDataJson, image);
         return ResponseEntity.status(201).body(savedPartner);
     }
 
